d12470062ce9da6ce61e86e1d4e96463873bf0b2
ChristianBarreto <christianbarreto19@fpuna.edu.py>
2025-06-08 20:57:10 -0300

    api post de clientes

--- src/app/api/cliente/route.js
+++ src/app/api/cliente/route.js
@@ -10,3 +10,46 @@ export async function GET() {
     return new Response('Error al obtener clientes', { status: 500 });
   }
 }
+
+export async function POST(req) {
+  try {
+    const data = await req.json();
+    const {
+      nombres,
+      apellidos,
+      documentonro,
+      direccion,
+      email,
+      telefono,
+      activo
+    } = data;
+    
+    // Obtiene el último id usado y suma 1
+    const result = await pool.query('SELECT COALESCE(MAX(id), 0) AS maxid FROM CLIENTES');
+    const newId = result.rows[0].maxid + 1;
+    
+    const query = `
+      INSERT INTO CLIENTES 
+        (id, nombres, apellidos, documentonro, direccion, email, telefono, activo)
+      VALUES 
+        ($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING *;
+    `;
+    const values = [
+      newId,
+      nombres,
+      apellidos,
+      documentonro,
+      direccion,
+      email,
+      telefono,
+      activo
+    ];
+
+    const { rows } = await pool.query(query, values);
+    return Response.json(rows[0]);
+  } catch (error) {
+    console.error('Error al insertar cliente:', error);
+    return new Response('Error al insertar cliente', { status: 500 });
+  }
+}
